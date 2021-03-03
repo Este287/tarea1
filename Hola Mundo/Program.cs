b1ca0cb27917abed027ecca7840fe93536670bd5
unknown <estefaniaroche123@gmail.com>
2021-03-03 00:59:09 -0600

    primer repositorio

---
+++ Hola Mundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        Console.WriteLine("Hola Mundo");
+             Console.WriteLine("Cual es tu nombre:");
+             var name = Console.ReadLine();
+             var date = DateTime.Now;
+             Console.WriteLine($"\nHola, {name}, on {date:d} at {date:t}!");
+             Console.Write("\nPress any key to exit...");
+             Console.ReadKey(true);
+
+        }
+    }
+}
